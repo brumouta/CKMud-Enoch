72f25009458ffb7b87a3b58dc90af873e1e456a5
Bruno Mouta <brumouta@gmail.com>
2024-08-07 01:01:01 +0100

    Fix elseif

--- src/triggers/Score Scraper/END_SCORE.lua
+++ src/triggers/Score Scraper/END_SCORE.lua
@@ -6,9 +6,9 @@ local function item_tier()
   local tier = 0
   if base_pl >= 3000000000 then
     tier = 8
-  if base_pl >= 1500000000 then
+  elseif base_pl >= 1500000000 then
     tier = 7
-  if base_pl >= 500000000 then
+  elseif base_pl >= 500000000 then
     tier = 6
   elseif base_pl >= 250000000 then
     tier = 5
